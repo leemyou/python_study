1b9ee3b5acf2afa1059e97a76323a31be76a64a2
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-04-01 23:22:24 +0900

    home rep5 commit

--- week5/rep1.py
+++ week5/rep1.py
@@ -1,3 +1,80 @@
-# https://www.kaggle.com/datasets/dell4010/wine-dataset
-# 데이터셋 와인 이거 참고
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from scipy.special import expit
+from scipy.special import softmax
 
+
+fish = pd.read_csv('https://bit.ly/fish_csv_data')
+# print(fish.head())
+
+print(pd.unique(fish['Species']))
+fish_input = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']].to_numpy()
+
+fish_target = fish['Species'].to_numpy()
+
+train_input, test_input, train_target, test_target = train_test_split(
+    fish_input, fish_target, random_state=42
+)
+
+ss = StandardScaler()
+ss.fit(train_input)
+
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+
+#로지스틱 회귀
+z = np.arange(-5, 5, 0.1)
+phi = 1 / (1 + np.exp(-z))
+
+plt.plot(z, phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+
+
+#로지스틱 회귀로 이진 분류 수행하기
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+print(lr.predict(train_bream_smelt[:5]))
+print(lr.predict_proba(train_bream_smelt[:5]))
+print(lr.classes_)
+
+print(lr.coef_, lr.intercept_)
+
+decisions = lr.decision_function(train_bream_smelt[:5])
+print(decisions)
+
+print(expit(decisions))
+
+
+#로지스틱 회귀로 다중 분류 수행하기
+lr = LogisticRegression(C=20, max_iter=1000)
+lr.fit(train_scaled, train_target)
+
+print(lr.score(train_scaled, train_target))
+print(lr.score(test_scaled, test_target))
+
+print(lr.predict(test_scaled[:5]))
+
+proba = lr.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=3))
+
+print(lr.classes_)
+
+print(lr.coef_.shape, lr.intercept_.shape)
+
+decision = lr.decision_function(test_scaled[:5])
+print(np.round(decision, decimals=2))
+
+proba = softmax(decision, axis=1)
+print(np.round(proba, decimals=3))
